d2d9850637909bf50a6f39125d94564fd1a51752
Ashish Gupta <ashvgupta938@gmail.com>
2022-10-22 18:42:53 -0400

    CSS Updates

--- client/WebsiteFunctions.js
+++ client/WebsiteFunctions.js
@@ -1,13 +1,11 @@
+// TODO: add a website function where sending the required data to the server runs toggleDisplay()
+// TODO: change the color of the buttons based on their ability to be pressed. if the button can be pressed, it should be #B4B4B4. otherwise, it should be "lightgray"
+
 window.addEventListener("DOMContentLoaded", () => {
     var toggleMenuBtn = document.getElementById('toggleMenu');
     //document.getElementById('toggleMenu');
     toggleMenuBtn.addEventListener('click', function() {
-        var menu = document.getElementById('menu');
-        if (menu.style.display === 'none') {
-            menu.style.display = 'block';
-        } else {
-            menu.style.display = 'none';
-        }
+        toggleDisplay();
     });
     
     document.querySelector(".init").addEventListener("click", async () => {
@@ -22,3 +20,8 @@ window.addEventListener("DOMContentLoaded", () => {
         CommandReceiver.connectTo(wsUrl);
     });
 });
+
+function toggleDisplay() {
+    var menu = document.getElementById('menu');
+    menu.style.display = menu.style.display === 'none' ? 'block' : 'none';
+}
