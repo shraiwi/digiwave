a6da405cbad45c892c77217dedfa0814d502bfd6
shranav@shraiwi.com <shranav@shraiwi.com>
2022-10-21 21:28:09 -0400

    shader.js skeleton

--- server/client/Shader.js
+++ server/client/Shader.js
@@ -1,1 +1,9 @@
- // empty :)
+// a simple sandboxed "shader" to determine whether or not to run the 
+
+function shader(x, y, time) {
+    // source code here
+}
+
+function createShader(source) {
+    return // shader
+}
